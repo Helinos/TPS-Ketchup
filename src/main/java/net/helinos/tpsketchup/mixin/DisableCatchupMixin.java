1164fb49328ca746f99fe22aaa00d9e7eaec244f
DontStarve72 <helinos67@protonmail.com>
2024-03-14 20:25:28 -0700

    Update to 1.20.4

--- src/main/java/net/helinos/tpsketchup/mixin/DisableCatchupMixin.java
+++ src/main/java/net/helinos/tpsketchup/mixin/DisableCatchupMixin.java
@@ -5,20 +5,23 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(MinecraftServer.class)
 public abstract class DisableCatchupMixin {
     @Shadow
-    private long nextTickTimestamp;
+    private long tickStartTimeNanos;
 
     @Shadow
-    private long timeReference;
+    private long tickEndTimeNanos;
 
-    @Redirect(method = "runServer()V", at = @At( value = "FIELD", target = "Lnet/minecraft/server/MinecraftServer;nextTickTimestamp:J", opcode = Opcodes.PUTFIELD))
-    private void disableCatchup(MinecraftServer minecraftServer, long time) {
-        long curTime = System.nanoTime();
-        this.nextTickTimestamp = this.timeReference = curTime / 1000000L + 50L;
+    @Inject(method = "runServer()V", at = @At( value = "FIELD", target = "Lnet/minecraft/server/MinecraftServer;tickEndTimeNanos:J", opcode = Opcodes.PUTFIELD), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void disableCatchup(CallbackInfo ci, long l) {
+        long currentTime = System.nanoTime();
+        tickStartTimeNanos = currentTime + l;
+        tickEndTimeNanos = tickStartTimeNanos;
     }
 }
